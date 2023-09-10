6985d07d4ac11c2b61f1b653622143a6a6d5d407
WalterMitty23 <23alexold23@gmail.com>
2023-09-10 17:02:47 -0300

    Copied class of lesson 1 to the directory of Lesson2 in order to complete task 2 with adding new methods into previous task.

---
+++ src/pro/sky/java/course1/lesson2/Author.java
@@ -0,0 +1,23 @@
+package pro.sky.java.course1.lesson2;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstname, String lastName) {
+        setAuthor(firstname, lastName);
+    }
+
+    public void setAuthor(String firstname, String lastName) {
+        this.firstName = firstname;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+
+    }
+}
--- src/pro/sky/java/course1/lesson2/Lesson2.java
+++ src/pro/sky/java/course1/lesson2/Lesson2.java
@@ -1,5 +1,32 @@
 package pro.sky.java.course1.lesson2;
 
+import pro.sky.java.course1.lesson1.Author;
+import pro.sky.java.course1.lesson1.Book;
+
 public class Lesson2 {
+    public static void main(String[] args) {
+        Author author1 = new Author("Fedor", "Dostoevskiy");
+        Author author2 = new Author("George", "Orwell");
+
+        Book book1 = new Book("Crime and Punishment", author1, 1866);
+        Book book2 = new Book("1984", author2, 1949);
+
+        book1.setBookName("-"); // can be used if requested
+
+        book1.setYearOfPublication(1961);
+
+        System.out.println("    Книга 1:");
+        System.out.println("Название: " + book1.getBookName());
+        System.out.println("Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book1.getYearOfPublication());
+
+        System.out.println("");
+
+        System.out.println("    Книга 2:");
+        System.out.println("Название: " + book2.getBookName());
+        System.out.println("Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName());
+        System.out.println("Год публикации: " + book2.getYearOfPublication());
+
+    }
 
 }
